a71e89ad0f608fabc880d0983e9517648f140222
j-irion <juliusirion@gmx.de>
2023-08-15 14:15:35 +0200

    Remove searchbar, add shopping cart icon

--- src/routes/root/root.jsx
+++ src/routes/root/root.jsx
@@ -3,6 +3,7 @@ import GitHubIcon from '../../assets/github.svg';
 import Logo from '../../assets/logo.png';
 import styles from './root.module.css';
 import { Outlet, Link } from 'react-router-dom';
+import Cart from '../../assets/cart.svg';
 
 export default function Root() {
   return (
@@ -10,17 +11,11 @@ export default function Root() {
       <div className={styles.container}>
         <header className={styles.header}>
           <img src={Logo} alt='Logo' />
-          <input
-            type='search'
-            name='searchbar'
-            id='searchbar'
-            className={styles.searchbar}
-          />
           <div className={styles.headerLinkGroup}>
             <Link to='../?index'>Home</Link>
             <Link to='../store'>Store</Link>
             <Link to='../cart'>
-              <img src='' alt='Shopping Cart' />
+              <img src={Cart} alt='Shopping Cart' />
             </Link>
           </div>
         </header>
